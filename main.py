efe3c21cdbb8ae6b061bb8f4641eec3b9aaadb0d
Afonso Castanheira de Abreu Nabais Baldo <up202004598@feup-b213d23.fe.up.pt>
2020-12-15 11:00:25 +0000

    Fazer com que o labirinto se mova

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+import time
 from PIL import Image
 
 
+# AINDA NAO SEI SE VOU LER O LABIRINTO POR UMA IMAGEM OU SE CRIO UM FCHEIRO TXT A PARTE
 def ler_labirinto(imagem):
     im = Image.open(imagem, 'r')
     width, height = im.size
@@ -34,24 +36,35 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Entombed')
+clock = pygame.time.Clock()
+
 running = True
+velocidade_y = 0.2
+camara_y = 0
 
 labirinto = ["1100000000"] * 8 + ler_labirinto("niveis/nivel1.png")
 
 while running:
     screen.fill(WHITE)
+    
+    # ver se o mapa ja acabou
+    if len(labirinto) * SQ_SIZE - SCREEN_HEIGHT <= camara_y:
+        running = False
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+            
+    dt = clock.tick()     # tempo que passou desde a ultima chamada
+    camara_y += dt*velocidade_y # mover camara
+    
     # desenhar labirinto
     y = 0  # y da linha
     for linha in labirinto:
         for x, i in enumerate(linha):
             if i == '1':
-                pygame.draw.rect(screen, VIOLETA, (x * SQ_SIZE, y, SQ_SIZE, SQ_SIZE))
-                pygame.draw.rect(screen, VIOLETA, ((2 * len(linha) - x - 1) * SQ_SIZE, y, SQ_SIZE, SQ_SIZE))
+                pygame.draw.rect(screen, VIOLETA, (x * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
+                pygame.draw.rect(screen, VIOLETA, ((2 * len(linha) - x - 1) * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
         y += SQ_SIZE
     pygame.display.update()
 
