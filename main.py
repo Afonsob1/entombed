6cb6482bb7c958ee0ee9151ff897a19c033b5433
Afonsob1 <afonsobaldo1@gmail.com>
2020-12-21 17:10:18 +0000

    Adicionar teclas para mover o jogador e detetar colisões com as paredes.

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ import pygame
 import time
 from PIL import Image
 
+# dimencoes
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SQ_SIZE = 40  # lado de cada retangulo
+
+# dimencoes do player
+PLAYER_SIZE = 20
+PLAYER_VELOCITY = 0.2
+
+SCREEN_LINHAS = SCREEN_HEIGHT // SQ_SIZE  # NUMERO DE LINHAS QUE CABEM NA JANELA
+
+# cores
+VERMELHO = (255, 0, 0)
+AZUL = (000, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0, 0)
+VIOLETA = (155, 155, 255)
+LARANJA = 0xff8c15
+
 
 # AINDA NAO SEI SE VOU LER O LABIRINTO POR UMA IMAGEM OU SE CRIO UM FCHEIRO TXT A PARTE
 def ler_labirinto(imagem):
@@ -19,82 +38,102 @@ def ler_labirinto(imagem):
     return l
 
 
-# dimencoes
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SQ_SIZE = 40  # lado de cada retangulo
-PLAYER_SIZE = 20
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-SCREEN_LINHAS = SCREEN_HEIGHT // SQ_SIZE  # NUMERO DE LINHAS QUE CABEM NA JANELA
+    pygame.display.set_caption('Entombed')
+    clock = pygame.time.Clock()
 
-# cores
-VERMELHO = (255, 0, 0)
-AZUL = (000, 0, 255)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0, 0)
-VIOLETA = (155, 155, 255)
-LARANJA = 0xff8c15 
+    # jogador
+    player = pygame.image.load('assets/jogador.png')
+    player.set_colorkey(WHITE)
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_h = player.get_height()
+    player_w = player.get_width()
 
-pygame.display.set_caption('Entombed')
-clock = pygame.time.Clock()
+    player_x, player_y = 100, 200
 
-# jogador
-player = pygame.image.load('assets/jogador.png')
-player.set_colorkey(WHITE)
+    running = True
+    velocidade_y = 0.05
+    camara_y = 0
 
-player_x , player_y = 100, 10
+    labirinto = ["1100000000"] * 12 + ler_labirinto("niveis/nivel1.png")
 
+    while running:
+        screen.fill(BLACK)
 
-running = True
-velocidade_y = 0.1
-camara_y = 0
-
-labirinto = ["1100000000"] * 12 + ler_labirinto("niveis/nivel1.png")
-
-while running:
-    screen.fill(BLACK)
-    
-    # ver se o mapa ja acabou
-    if len(labirinto) * SQ_SIZE - SCREEN_HEIGHT <= camara_y:
-        running = False
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # ver se o mapa ja acabou
+        if len(labirinto) * SQ_SIZE - SCREEN_HEIGHT <= camara_y:
             running = False
-            
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_x -= 0.5
-    if keys[pygame.K_RIGHT]:
-        player_x += 0.5
-    if keys[pygame.K_UP]:
-        player_y -= 0.5
-    if keys[pygame.K_DOWN]:
-        player_y += 0.5
-            
-    
-            
-    dt = clock.tick()     # tempo que passou desde a ultima chamada
-    camara_y += dt*velocidade_y # mover camara
-    
-    ############### rendering ##############   
-    
-    # desenhar labirinto    
-    y = 0  # y da linha
-    for linha in labirinto:
-        for x, i in enumerate(linha):
-            if i == '1':
-                pygame.draw.rect(screen, LARANJA, (x * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
-                pygame.draw.rect(screen, LARANJA, ((2 * len(linha) - x - 1) * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
-        y += SQ_SIZE
-        
-        
-    # desenhar o jogador
-    screen.blit(player, (player_x, player_y))
-    
-    pygame.display.update()
 
-pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # ver se jogador ultrapassou limites do ecra
+        if player_y < 0:
+            running = False
+        elif player_y > SCREEN_HEIGHT - player.get_height():
+            player_y = SCREEN_HEIGHT - player.get_height()
+
+        add_x = 0
+        add_y = 0
+
+        dt = clock.tick()  # tempo que passou desde a ultima chamada
+        camara_y += dt * velocidade_y  # mover camara
+        player_y -= dt * velocidade_y  # mover jogador
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            add_x -= dt * PLAYER_VELOCITY
+        if keys[pygame.K_RIGHT]:
+            add_x += dt * PLAYER_VELOCITY
+        if keys[pygame.K_UP]:
+            add_y -= dt * PLAYER_VELOCITY
+        if keys[pygame.K_DOWN]:
+            add_y += dt * PLAYER_VELOCITY
+
+        y = 0  # y da linha
+        for linha in labirinto:
+            for x, i in enumerate(linha):
+                if i == '1':
+                    rect = pygame.Rect(x * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE)
+
+                    # Quando fica no chao ele andava mais lento se fizer isto isso já nao acontece
+                    player_rect = pygame.Rect(player_x + add_x, player_y+1, player_w, player_h)
+                    if player_rect.colliderect(rect):
+                        player_rect = pygame.Rect(player_x + add_x, player_y-1, player_w, player_h)
+                        if player_rect.colliderect(rect):
+                            add_x = 0
+
+                    player_rect = pygame.Rect(player_x, player_y + add_y, player_w, player_h)
+                    if player_rect.colliderect(rect):
+                        add_y = 0
+
+            y += SQ_SIZE
+
+        player_x += add_x
+        player_y += add_y
+
+        ############## rendering ##############
+
+        # desenhar labirinto
+        y = 0  # y da linha
+        for linha in labirinto:
+            for x, i in enumerate(linha):
+                if i == '1':
+                    pygame.draw.rect(screen, LARANJA, (x * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
+                    pygame.draw.rect(screen, LARANJA, ((2 * len(linha) - x - 1) * SQ_SIZE, y - camara_y, SQ_SIZE, SQ_SIZE))
+
+            y += SQ_SIZE
+
+        # desenhar o jogador
+        screen.blit(player, (player_x, player_y))
+
+        pygame.display.update()
+
+    pygame.quit()
+
+
+main()
